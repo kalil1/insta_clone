8eb8224599215fb0d331433a796e9c0538e96d55
Kalil <35199743+kalil1@users.noreply.github.com>
2025-05-13 23:06:22 -0400

    Update production.rb (#34)
    * Update production.rb
    
    * Update package.json
    
    * Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,12 +23,12 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.js_compressor = nil
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "insta_clone_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.assets.initialize_on_precompile = false
+  # config.assets.initialize_on_precompile = false
   
 
   # Ignore bad email addresses and do not raise email delivery errors.
