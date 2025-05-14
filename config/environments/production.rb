21f5aa3a41ccc564a326ebae8e95b4b6b0c73219
kalil1 <wkalil77@gmail.com>
2025-05-14 03:19:42 -0400

    api only

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
+  config.assets.digest = false
   config.assets.js_compressor = nil
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -64,7 +65,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "insta_clone_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  # config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = false
   
 
   # Ignore bad email addresses and do not raise email delivery errors.
