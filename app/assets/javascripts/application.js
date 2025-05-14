28771b5d29f42c424c3e72c31c8f0a75ca9d31f1
kalil1 <wkalil77@gmail.com>
2025-05-14 04:35:23 -0400

    removing more turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,55 +10,51 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
 
-const like = (id) => {
-  if (document.querySelector("#like" + id).style.display == "inline-block") {
-    document.querySelector("#like" + id).style.display = "none"
-    document.querySelector("#unlike" + id).style.display = "inline-block"
-  } else if (document.querySelector("#unlike" + id).style.display == "inline-block") {
-    document.querySelector("#like" + id).style.display = "inline-block"
-    document.querySelector("#unlike" + id).style.display = "none"
-  };
-};
+// const like = (id) => {
+//   if (document.querySelector("#like" + id).style.display == "inline-block") {
+//     document.querySelector("#like" + id).style.display = "none"
+//     document.querySelector("#unlike" + id).style.display = "inline-block"
+//   } else if (document.querySelector("#unlike" + id).style.display == "inline-block") {
+//     document.querySelector("#like" + id).style.display = "inline-block"
+//     document.querySelector("#unlike" + id).style.display = "none"
+//   };
+// };
 
-const follow = (id) => {
-  if (document.querySelector("#is-following" + id).style.display == "block") {
-    document.querySelector("#is-following" + id).style.display = "none"
-    document.querySelector("#is-not-following" + id).style.display = "block"
-  } else if (document.querySelector("#is-not-following" + id).style.display == "block") {
-    document.querySelector("#is-following" + id).style.display = "block"
-    document.querySelector("#is-not-following" + id).style.display = "none"
-  };
-};
+// const follow = (id) => {
+//   if (document.querySelector("#is-following" + id).style.display == "block") {
+//     document.querySelector("#is-following" + id).style.display = "none"
+//     document.querySelector("#is-not-following" + id).style.display = "block"
+//   } else if (document.querySelector("#is-not-following" + id).style.display == "block") {
+//     document.querySelector("#is-following" + id).style.display = "block"
+//     document.querySelector("#is-not-following" + id).style.display = "none"
+//   };
+// };
 
-function readURL(input) {
-     if (input.files && input.files[0]) {
-      var reader = new FileReader();
-      reader.onload = function (e) {
-      document.querySelector('#blah').src = e.target.result;
-      };
-     reader.readAsDataURL(input.files[0]);
-    }
-  }
+// function readURL(input) {
+//      if (input.files && input.files[0]) {
+//       var reader = new FileReader();
+//       reader.onload = function (e) {
+//       document.querySelector('#blah').src = e.target.result;
+//       };
+//      reader.readAsDataURL(input.files[0]);
+//     }
+//   }
 
-  const comment = (post_id) => {
-  const content_response = document.getElementById("content_response" + post_id);
-  content_response.style.display = "none";
-  let req = new XMLHttpRequest;
-  let url = "/post/comments";
-  req.open('post', url, true);
-  req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  let token = document.getElementsByName("csrf-token")[0].content;
-  req.setRequestHeader('X-CSRF-Token', token);
-  req.onreadystatechange = function() {
-    if (req.readyState === XMLHttpRequest.DONE) {
-      content_response.innerHTML = req.responseText;
-      content_response.style.display = 'block';
-    }
-  };
-  req.send('&id=' + post_id);
-};
+//   const comment = (post_id) => {
+//   const content_response = document.getElementById("content_response" + post_id);
+//   content_response.style.display = "none";
+//   let req = new XMLHttpRequest;
+//   let url = "/post/comments";
+//   req.open('post', url, true);
+//   req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+//   let token = document.getElementsByName("csrf-token")[0].content;
+//   req.setRequestHeader('X-CSRF-Token', token);
+//   req.onreadystatechange = function() {
+//     if (req.readyState === XMLHttpRequest.DONE) {
+//       content_response.innerHTML = req.responseText;
+//       content_response.style.display = 'block';
+//     }
+//   };
+//   req.send('&id=' + post_id);
+// };
