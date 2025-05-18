00194ccc747d4518cabb62233cc9dcd8841aa77f
kalil1 <wkalil77@gmail.com>
2025-05-18 19:22:48 -0400

    fix: merge conflict

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,7 +1,3 @@
 class ProfileSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
   attributes :id, :name, :website, :bio, :phone, :profile_pic
-=======
-  attributes :id, :name, :website, :bio, :phone
->>>>>>> 61d689a007d959dcac1fd0a1e66385ce97812313
 end
