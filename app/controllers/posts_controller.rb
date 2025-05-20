c0d3f26f5be3e51abf1eefc5cd94becb035e6b4e
kalil1 <wkalil77@gmail.com>
2025-05-20 00:32:07 -0400

    fix: limit home page post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :update, :destroy]
+  # Will make more complex later when user auth is a thing in this project
 
   def index
-    posts = Post.includes(:profile, comments: :profile).order(created_at: :desc)
+    posts = Post.includes(:profile, comments: :profile).order(created_at: :desc).limit(15)
     render json: posts, each_serializer: PostSerializer
   end
 
@@ -11,7 +12,6 @@ class PostsController < ApplicationController
   end
 
   def explore_page
-    # Will make more complex later when user auth is a thing in this project
     posts = Post.includes(:profile, comments: :profile).order(created_at: :desc).limit(30)
     render json: posts, each_serializer: PostSerializer
   end
