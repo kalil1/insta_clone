a0d0ad51b6eac0b3f464e79b38e4240f761013be
Kalil <35199743+kalil1@users.noreply.github.com>
2025-05-20 00:11:09 -0400

    Ticket 007 data generation (#73)
    * chore: new_configuration
    
    * feat: post & profile serializers
    
    * feat: user endpoints
    
    * update: post serialization
    
    * database change
    
    * make it better
    
    * heroku-24 stack
    
    * heroku-24 stack #01
    
    * heroku-24 stack #03
    
    * database update
    
    * bin update
    
    * bin update
    
    * Add encrypted credentials
    
    * Add
    
    * heroku update
    
    * heroku update
    
    * Fix: Add bootsnap and update lockfile
    
    * turbolinks
    
    * removed coffee
    
    * api only
    
    * api only controllers
    
    * api only
    
    * assets removed
    
    * gem 'carrierwave'
    
    * no_image problem
    
    * removing more turbolinks
    
    * Explicitly link no_image.png in manifest.js
    
    * moved images
    
    * moved images
    
    * moved copied
    
    * adjusted gitignore
    
    * adjusted gitignore
    
    * fix:reserialized
    
    * fix: likes
    
    * fix: added profile id to serializer
    
    * feat: upload to s3
    
    * feat: gnerate random post
    
    * feat: fix post images
    
    * feat: fix profile_pic images
    
    * fix: url hash to string
    
    * fix: serializer
    
    * fix: removed carrierwave from post model
    
    * fix: merge conflict
    
    * generate likes & comments
    
    * fix: explore_page
    
    * feat: profile search

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,19 +1,16 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :update, :destroy]
 
-  # GET /profiles
   def index
     @profiles = Profile.all
     render json: @profiles
   end
 
-  # GET /profiles/1
   def show
     @posts = Post.where(profile_id: @profile.id).order(created_at: :desc)
     render json: { profile: @profile, posts: @posts }
   end
 
-  # POST /profiles
   def create
     @profile = Profile.new(profile_params)
     @profile.user = current_user
@@ -25,7 +22,16 @@ class ProfilesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /profiles/1
+  def search
+    if params[:query].present?
+      search_term = params[:query].downcase
+      matching_profiles = Profile.where('LOWER(name) LIKE ?', "%#{search_term}%").limit(5)
+      render json: { profiles: matching_profiles }, status: :ok
+    else
+      render json: { error: 'Query parameter is required' }, status: :bad_request
+    end
+  end
+
   def update
     if @profile.update(profile_params.merge(profile_pic: profile_params[:profile_pic]))
       render json: @profile, status: :ok
@@ -34,7 +40,6 @@ class ProfilesController < ApplicationController
     end
   end
 
-  # DELETE /profiles/1
   def destroy
     @profile.destroy
     head :no_content
@@ -42,12 +47,10 @@ class ProfilesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_profile
     @profile = Profile.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def profile_params
     params.require(:profile).permit(:name, :website, :bio, :phone, :profile_pic)
   end
